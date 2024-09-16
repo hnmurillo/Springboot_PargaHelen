70d9258299999ba8c22a233f44e825bb15f516f7
HelenMurillo <156430545+hnmurillo@users.noreply.github.com>
2024-09-16 08:52:05 -0500

    feat :tada:  inicio proyecto
    inicio del día uno en Spring boot

---
+++ dia1/ejercicio_dia1/src/main/java/com/example/dia1/Dia1Application.java
@@ -0,0 +1,13 @@
+package com.example.dia1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dia1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dia1Application.class, args);
+	}
+// dsdsdsdsds
+}
---
+++ dia1/ejercicio_dia1/src/test/java/com/example/dia1/Dia1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dia1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dia1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
